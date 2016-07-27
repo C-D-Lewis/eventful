b17ecd9de3b41e5d26cd0f4743e1fe511ea670fe
Chris Lewis <Bonsitm@gmail.com>
2016-07-27 17:41:28 +0100

    Add config of update interval

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <pebble.h>
 
+#include "modules/data.h"
+
 #include <pebble-dash-api/pebble-dash-api.h>
 #include <pebble-events/pebble-events.h>
 
@@ -30,16 +32,27 @@ static void get_unread_count_callback(DataType type, DataValue value) {
 static void tick_handler(struct tm *tick_time, TimeUnits changed) {
   main_window_set_time(tick_time);
 
+  const int freq = data_get_update_frequency();
   const int mins = tick_time->tm_min;
-  if((mins % 15 == 0) || !s_first_request_made) {
+  if((mins % freq == 0) || !s_first_request_made) {
     s_first_request_made = true;
 
     dash_api_get_data(DataTypeUnreadSMSCount, get_unread_count_callback);
   }
 }
 
+static void in_recv_handler(DictionaryIterator *iter, void *context) {
+  Tuple *update_t = dict_find(iter, MESSAGE_KEY_UpdateFrequency);
+  if(update_t) {
+    data_set_update_frequency(update_t->value->int32);
+  }
+}
+
 static void init() {
+  data_init();
   main_window_push();
+
+  events_app_message_register_inbox_received(in_recv_handler, NULL);
 
   char *app_name = "Eventful";
   dash_api_init(app_name, error_callback);
